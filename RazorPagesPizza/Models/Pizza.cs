278924fc3dd5fc5b0fa70cc94ed017b27202681c
tobias innleggen <tin017@uib.no>
2022-05-22 00:02:10 +0200

    Finished the tutorial

--- RazorPagesPizza/Models/Pizza.cs
+++ RazorPagesPizza/Models/Pizza.cs
@@ -6,11 +6,14 @@ public class Pizza
 {
     public int Id { get; set; }
 
+
+    //This indicates that the following value must have a value
     [Required]
     public string? Name { get; set; }
     public PizzaSize Size { get; set; }
     public bool IsGlutenFree { get; set; }
 
+    //This says the value have to be in the given range
     [Range(0.01, 9999.99)]
     public decimal  Price { get; set; }
 }
--- RazorPagesPizza/Services/PizzaService.cs
+++ RazorPagesPizza/Services/PizzaService.cs
@@ -1,6 +1,47 @@
-﻿namespace RazorPagesPizza.Services
+﻿using RazorPagesPizza.Models;
+
+namespace RazorPagesPizza.Services;
+public class PizzaService
 {
-    public class Class
+    //this makes a new list that is to be filled with pizza objects
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    //This constructor puts two new pizza objects in the pizzalist
+    static PizzaService()
     {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.    Large, IsGlutenFree = false },
+            new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
+        };
+    }
+
+    //this function returns all the objects in in the pizzalist
+    public static List<Pizza> GetAll() => Pizzas;
+
+    //This function returns a spesiffic pizza element and it if cant find it it retruns som default value
+    public static Pizza? Get(int Id ) => Pizzas.FirstOrDefault( p => p.Id == Id);
+
+    //This function increases the id value of the pizza input and thjen adds it to the pizzas list
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    //This function deletes the pizza with a given id, if the id is not found it does nothing
+    public static void Delete(int Id)
+    {
+        var pizza = Get(Id);
+        if(pizza is null) { return; }
+        Pizzas.Remove(pizza);
+    }
+
+    //Dont know what this does
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if(index == -1) { return; }
+        Pizzas[index] = pizza;
     }
 }
